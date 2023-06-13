b88c49c3ef923f7acdc191be1d7da7d952086906
bingAbdi <136521723+bingAbdi@users.noreply.github.com>
2023-06-14 02:00:23 +0300

    Update 24backup.php

--- 24backup.php
+++ 24backup.php
@@ -16,10 +16,45 @@ add_action('wp', 'schedule_site_backup');
 
 function perform_site_backup() {
     // Code to perform the site backup
-    // Replace this with your backup logic
-    // Example: You can use a plugin like UpdraftPlus or create your custom backup routine
+
+// Define the source directory and backup filename
+$sourceDir = '/path/to/source/directory';
+$backupFilename = 'site_backup_' . date('Y-m-d') . '.zip';
+
+// Create a new ZIP archive
+$zip = new ZipArchive();
+if ($zip->open($backupFilename, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
+    // Create a recursive directory iterator to traverse all files and directories
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($sourceDir),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+
+    foreach ($files as $name => $file) {
+        // Skip directories (they are automatically created in the archive)
+        if (!$file->isDir()) {
+            // Get the relative path of the file (remove the source directory)
+            $relativePath = substr($name, strlen($sourceDir) + 1);
+
+            // Add the file to the archive with its relative path
+            $zip->addFile($name, $relativePath);
+        }
+    }
+
+    // Close the ZIP archive
+    $zip->close();
+
+    echo 'Site backup created successfully.';
+} else {
+    echo 'Failed to create site backup.';
+}
+
+
+
+
     // For simplicity, we'll just log a message in this example
     error_log('Site backup performed on ' . date('Y-m-d H:i:s'));
 }
 
 add_action('site_backup_event', 'perform_site_backup');
+?>
