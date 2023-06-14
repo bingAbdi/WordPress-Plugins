379733119acf63833d53e6d97c5a8df7d43307d9
bingAbdi <136521723+bingAbdi@users.noreply.github.com>
2023-06-14 03:23:26 +0300

    Update EmageOptimize.php

--- EmageOptimize.php
+++ EmageOptimize.php
@@ -1,5 +1,4 @@
 <?php
-    
 /*
 Plugin Name: EmageOptimize 
 Description: High-quality image resizing plugin with customization options, watermarking, and support for retina displays.
@@ -46,6 +45,61 @@ function emage_override_image_sizes($sizes) {
 add_filter('intermediate_image_sizes_advanced', 'emage_override_image_sizes');
 
 // Serve high-resolution images for retina displays
+function emage_serve_retina_images($image, $attachment_id, $size) {
+    $emage_option = get_option('emage_option');
+    if ($emage_option === 'maximum' && function_exists('wp_get_attachment_image_src')) {
+        $retina_image = wp_get_attachment_image_src($attachment_id, $size . '_retina');
+        if ($retina_image && isset($retina_image[0])) {
+            $image[0] = $retina_image[0];
+            $image[1] = $retina_image[1];
+            $image[2] = $retina_image[2];
+        }
+    }
+    return $image;
+}
+add_filter('wp_get_attachment_image_src', 'emage_serve_retina_images', 10, 3);
+
+// Render the configuration page
+function emage_render_settings_page() {
+    // Check user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    // Save settings
+    if (isset($_POST['emage_save_settings'])) {
+        // Perform validation and save settings here
+        $emage_option = isset($_POST['emage_option']) ? $_POST['emage_option'] : '';
+        update_option('emage_option', $emage_option);
+        $emage_format = isset($_POST['emage_format']) ? sanitize_text
+
+// Serve high-rfunction emage_override_image_sizes($sizes) {
+    $emage_option = get_option('emage_option');
+    if ($emage_option === 'maximum') {
+        $sizes['large'] = array(
+            'width' => get_option('large_size_w') * 2,
+            'height' => get_option('large_size_h') * 2,
+            'crop' => false,
+        );
+        $sizes['medium'] = array(
+            'width' => get_option('medium_size_w') * 2,
+            'height' => get_option('medium_size_h') * 2,
+            'crop' => false,
+        );
+        $sizes['thumbnail'] = array(
+            'width' => get_option('thumbnail_size_w') * 2,
+            'height' => get_option('thumbnail_size_h') * 2,
+            'crop' => false,
+        );
+    }
+    return $sizes;
+}
+add_filter('intermediate_image_sizes_advanced', 'emage_override_image_sizes');
+
+// Serve high-resolution images for retina displays
+function emage_serve_retina_images($image_url, $attachment_id, $size) {
+    $emage_option = get_option('emage_option');
+    if ($emage_option === 'maximum' && function_existsesolution images for retina displays
 function emage_serve_retina_images($image_url, $attachment_id, $size) {
     $emage_option = get_option('emage_option');
     if ($emage_option === 'maximum' && function_exists('wp_get_attachment_image_src')) {
